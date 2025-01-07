283a3cb2487ec9ef0b4e7cb23627b255fffda09d
Kevin <soccerkevint11@gmail.com>
2025-01-07 11:07:24 -0800

    keep relational table names plural

--- src/db/migrations/20250107070422_create_roles_users.ts
+++ src/db/migrations/20250107070422_create_roles_users.ts
@@ -1,7 +1,7 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('role_users', (table) => {
+  return knex.schema.createTable('roles_users', (table) => {
     table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE');
     table.integer('role_id').unsigned().references('id').inTable('roles').onDelete('CASCADE');
     table.primary(['user_id', 'role_id']);
@@ -10,6 +10,6 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTableIfExists('role_users');
+  return knex.schema.dropTableIfExists('roles_users');
 }
 
