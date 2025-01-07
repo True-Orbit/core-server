99db89d587f2f9575f0a45ecbb6f20a4a14d50d2
Kevin <soccerkevint11@gmail.com>
2025-01-07 12:10:37 -0800

    add user and roles resources

---
+++ src/resources/users/index.ts
@@ -0,0 +1,2 @@
+export * from "./models";
+export { default as router } from "./routes";
---
+++ src/resources/users/router.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { dbConnection } from '@/db';
+
+const router = express.Router();
+
+router.get('/users', async (req, res, next) => {
+  const result = dbConnection.raw('SELECT 1');
+  console.log(result);
+  res.send('Welcome to the Express Server!');
+});
+
+export default router;
