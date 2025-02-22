a3e2ca73f3c5835c5eda19fa091540691f8a0f54
Kevin <soccerkevint11@gmail.com>
2025-02-22 13:04:39 -0800

    prettier

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -6,6 +6,6 @@ import config from '@/root/knexfile';
 
 const env = process.env.NODE_ENV || 'local';
 
-console.log('config: ', config[env])
+console.log('config: ', config[env]);
 
 export const dbConnection = knex(config[env]);
