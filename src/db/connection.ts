5ba9ab4f881e2f53190af7fddde40b55f5119e50
Kevin <soccerkevint11@gmail.com>
2025-02-21 23:35:46 -0800

    log out cwd

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,15 @@
 import knex from 'knex';
+
 import dotenv from 'dotenv';
+
 dotenv.config();
+
+console.log('cwd: ', process.cwd());
 
 import config from '../../knexfile';
 
 const env = process.env.NODE_ENV || 'local';
+
+console.log('config: ', config[env])
 
 export const dbConnection = knex(config[env]);
