c2879e2ce6068e147c392b3ca97104791618573a
Kevin <soccerkevint11@gmail.com>
2025-02-24 11:52:53 -0800

    add npx in docker

--- scripts/unpackSecrets.sh
+++ scripts/unpackSecrets.sh
@@ -10,7 +10,7 @@ if [ -n "$RDS_DEVELOPMENT_SECRETS" ]; then
 fi
 
 if [ -n "$CORE_SERVER_SECRETS" ]; then
-  echo "Parsing WEB_SERVICE_SECRETS and exporting variables..."
+  echo "Parsing CORE_SERVER_SECRETS and exporting variables..."
   eval "$(
     echo "$CORE_SERVER_SECRETS" \
       | jq -r 'to_entries | map("export " + .key + "=" + (.value|@sh)) | .[]'
