4ccff8284456261ad7cbc75823773853f29d44de
Kevin <soccerkevint11@gmail.com>
2025-03-09 21:37:01 -0700

    working

--- src/middleware/requireUserAuth.ts
+++ src/middleware/requireUserAuth.ts
@@ -7,27 +7,31 @@ import { models as accessTokenModels } from '@/resources/accessTokens';
 const DOMAIN = process.env.DOMAIN!;
 const AUTH_SECRET = process.env.AUTH_SECRET!;
 
-const csrfTokenName = 'X-CSRF-Token';
+const csrfTokenName = 'x-csrf-token';
 const CSRF_SECRET = process.env.CSRF_SECRET!;
 
 const corsOptions = {
   origin: DOMAIN,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'X-CSRF-Token'],
+  allowedHeaders: ['Content-Type', csrfTokenName],
   credentials: true,
 };
 
 export const requireUserAuth = (req: Request, res: Response, next: NextFunction) => {
   cors(corsOptions)(req, res, (corsErr) => {
     if (corsErr) return next(corsErr);
-
     const accessToken = req.cookies.accessToken;
     const csrfToken = req.headers[csrfTokenName] as string;
 
     try {
       // just verify that a valid csrf token is present
       jwt.verify(csrfToken, CSRF_SECRET);
-      
+    } catch (error) {
+      console.error(error);
+      return res.status(401).json({ error: 'Invalid csrf token' });
+    }
+
+    try {
       const decoded: accessTokenModels.Data = jwt.verify(accessToken, AUTH_SECRET) as accessTokenModels.Data;
       req.authUser = decoded;
 
