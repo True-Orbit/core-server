d217aefb563e75aba677e26da288646cf0d75b1e
Kevin <soccerkevint11@gmail.com>
2025-03-09 20:25:35 -0700

    check for csrf token

--- src/middleware/requireUserAuth.ts
+++ src/middleware/requireUserAuth.ts
@@ -1,21 +1,40 @@
 import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
+import cors from 'cors';
 
 import { models as accessTokenModels } from '@/resources/accessTokens';
 
+const DOMAIN = process.env.DOMAIN!;
 const AUTH_SECRET = process.env.AUTH_SECRET!;
 
+const csrfTokenName = 'X-CSRF-Token';
+const CSRF_SECRET = process.env.CSRF_SECRET!;
+
+const corsOptions = {
+  origin: DOMAIN,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'X-CSRF-Token'],
+  credentials: true,
+};
+
 export const requireUserAuth = (req: Request, res: Response, next: NextFunction) => {
-  console.log('requireUserAuth cookies', req.cookies);
-  const accessToken = req.cookies.accessToken;
+  cors(corsOptions)(req, res, (corsErr) => {
+    if (corsErr) return next(corsErr);
 
-  try {
-    const decoded: accessTokenModels.Data = jwt.verify(accessToken, AUTH_SECRET) as accessTokenModels.Data;
-    req.authUser = decoded;
+    const accessToken = req.cookies.accessToken;
+    const csrfToken = req.headers[csrfTokenName] as string;
 
-    next();
-  } catch (err) {
-    console.error(err);
-    return res.status(401).json({ error: 'Invalid authentication token' });
-  }
+    try {
+      // just verify that a valid csrf token is present
+      jwt.verify(csrfToken, CSRF_SECRET);
+      
+      const decoded: accessTokenModels.Data = jwt.verify(accessToken, AUTH_SECRET) as accessTokenModels.Data;
+      req.authUser = decoded;
+
+      next();
+    } catch (err) {
+      console.error(err);
+      return res.status(401).json({ error: 'Invalid authentication token' });
+    }
+  });
 };
